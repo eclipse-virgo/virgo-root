2b5249e25311e123c6c90071f1e4cf49dcfca7ef
Glyn Normington <gnormington@vmware.com>
2010-12-31 10:17:40 +0000

    bug 330776: add user region factory bundle

---
+++ org.eclipse.virgo.kernel.userregionfactory/src/main/java/org/eclipse/virgo/kernel/userregionfactory/Activator.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * This file is part of the Virgo Web Server.
+ *
+ * Copyright (c) 2010 Eclipse Foundation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SpringSource, a division of VMware - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
+package org.eclipse.virgo.kernel.userregionfactory;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ *{@link Activator} initialises the user region factory bundle.
+ * <p />
+ * 
+ * <strong>Concurrent Semantics</strong><br />
+ * 
+ * Thread safe.
+ * 
+ */
+public final class Activator implements BundleActivator {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void start(BundleContext context) throws Exception {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void stop(BundleContext context) throws Exception {
+    }
+
+}
