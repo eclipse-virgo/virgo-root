a279ddb207ea8822c159f5831d1fa471e8c62cbb
Glyn Normington <gnormington@vmware.com>
2011-10-04 15:24:40 +0100

    bug 353996: test configuration in ArtifactModel

--- org.eclipse.virgo.kernel.deployer.test/src/test/java/org/eclipse/virgo/kernel/deployer/test/JmxArtifactModelTests.java
+++ org.eclipse.virgo.kernel.deployer.test/src/test/java/org/eclipse/virgo/kernel/deployer/test/JmxArtifactModelTests.java
@@ -32,16 +32,18 @@ import org.junit.Test;
 import org.osgi.framework.ServiceReference;
 
 /**
- * Test deployed artifacts show up correctly on JMX.
+ * Test deployed artifacts show up correctly in JMX, in both the older Model and the newer, region-aware ArtifactModel.
  * 
  */
 public class JmxArtifactModelTests extends AbstractDeployerIntegrationTest {
 
-    private static final String CONFIGURATION_VERSION = "0.0.0";
+    private static final String GLOBAL_USER_REGION = "global";
+
+    private static final String CONFIGURATION_TYPE = "configuration";
 
     private static final String CONFIGURATION_NAME = "t";
 
-    private static final String CONFIGURATION_TYPE = "configuration";
+    private static final String CONFIGURATION_VERSION = "0.0.0";
 
     private ServiceReference<ApplicationDeployer> appDeployerServiceReference;
 
@@ -62,9 +64,9 @@ public class JmxArtifactModelTests extends AbstractDeployerIntegrationTest {
             this.context.ungetService(this.appDeployerServiceReference);
         }
     }
-
+    
     @Test
-    public void testDeployConfig() throws Exception {
+    public void testConfigModel() throws Exception {
         File file = new File("src/test/resources/configuration.deployment/t.properties");
 
         DeploymentIdentity deploymentIdentity = this.appDeployer.deploy(file.toURI());
@@ -73,11 +75,34 @@ public class JmxArtifactModelTests extends AbstractDeployerIntegrationTest {
 
         assertArtifactState(CONFIGURATION_TYPE, CONFIGURATION_NAME, CONFIGURATION_VERSION, "ACTIVE");
 
-        TabularDataSupport attribute = (TabularDataSupport) getMBeanServerConnection().getAttribute(
+        TabularDataSupport attribute = (TabularDataSupport) this.mBeanServerConnection.getAttribute(
             getObjectName(CONFIGURATION_TYPE, CONFIGURATION_NAME, CONFIGURATION_VERSION), "Properties");
         assertEquals("b", getStringValue(attribute, "a"));
         assertEquals("d", getStringValue(attribute, "c"));
+
+        this.appDeployer.undeploy(deploymentIdentity);
     }
+
+    @Test
+    public void testConfigArtifactModel() throws Exception {
+        File file = new File("src/test/resources/configuration.deployment/t.properties");
+
+        DeploymentIdentity deploymentIdentity = this.appDeployer.deploy(file.toURI());
+
+        assertDeploymentIdentityEquals(deploymentIdentity, "t.properties", CONFIGURATION_TYPE, CONFIGURATION_NAME, "0");
+
+        assertArtifactState(GLOBAL_USER_REGION, CONFIGURATION_TYPE, CONFIGURATION_NAME, CONFIGURATION_VERSION, "ACTIVE");
+
+        TabularDataSupport attribute = (TabularDataSupport) this.mBeanServerConnection.getAttribute(
+            getObjectName(GLOBAL_USER_REGION, CONFIGURATION_TYPE, CONFIGURATION_NAME, CONFIGURATION_VERSION), "Properties");
+        assertEquals("b", getStringValue(attribute, "a"));
+        assertEquals("d", getStringValue(attribute, "c"));
+
+        this.appDeployer.undeploy(deploymentIdentity);
+
+    }
+    
+    //TODO: test other artefact types
 
     private String getStringValue(TabularDataSupport attribute, String key) {
         Object[] keys = { key };
@@ -99,6 +124,23 @@ public class JmxArtifactModelTests extends AbstractDeployerIntegrationTest {
 
     protected static ObjectName getObjectName(String type, String name, String version) throws MalformedObjectNameException {
         return new ObjectName(String.format("org.eclipse.virgo.kernel:type=Model,artifact-type=%s,name=%s,version=%s", type, name, version));
+    }
+
+    private void assertArtifactState(String region, String type, String name, String version, String state) throws MalformedObjectNameException,
+        IOException, Exception {
+        assertArtifactExists(region, type, name, version);
+        assertEquals(String.format("Artifact %s:%s:%s:%s is not in state %s", region, type, name, version, state), state,
+            this.mBeanServerConnection.getAttribute(getObjectName(region, type, name, version), "State"));
+    }
+
+    private void assertArtifactExists(String region, String type, String name, String version) throws IOException, Exception, MalformedObjectNameException {
+        assertTrue(String.format("Artifact %s:%s:%s:%s does not exist", region, type, name, version),
+            this.mBeanServerConnection.isRegistered(getObjectName(region, type, name, version)));
+    }
+
+    protected static ObjectName getObjectName(String region, String type, String name, String version) throws MalformedObjectNameException {
+        return new ObjectName(String.format("org.eclipse.virgo.kernel:type=ArtifactModel,region=%s,artifact-type=%s,name=%s,version=%s", region,
+            type, name, version));
     }
 
     private static MBeanServerConnection getMBeanServerConnection() throws Exception {
