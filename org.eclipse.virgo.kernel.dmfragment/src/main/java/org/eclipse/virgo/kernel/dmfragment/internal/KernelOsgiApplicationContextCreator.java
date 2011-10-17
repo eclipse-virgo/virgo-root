7c7c26987a56cbd812eaa098a513d0be211636a6
Glyn Normington <gnormington@vmware.com>
2011-10-17 12:25:53 +0100

    bug 360965: introduce OsgiApplicationContextCreator for user region

---
+++ org.eclipse.virgo.kernel.dmfragment/src/main/java/org/eclipse/virgo/kernel/dmfragment/internal/KernelOsgiApplicationContextCreator.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2011 VMware Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   VMware Inc. - initial contribution
+ *******************************************************************************/
+
+package org.eclipse.virgo.kernel.dmfragment.internal;
+
+import org.osgi.framework.BundleContext;
+import org.springframework.osgi.context.DelegatedExecutionOsgiBundleApplicationContext;
+import org.springframework.osgi.extender.support.DefaultOsgiApplicationContextCreator;
+
+/**
+ * {@link KernelOsgiApplicationContextCreator} creates user region application contexts.
+ * <p />
+ * 
+ * <strong>Concurrent Semantics</strong><br />
+ * Thread safe.
+ */
+final class KernelOsgiApplicationContextCreator extends DefaultOsgiApplicationContextCreator {
+
+    /** 
+     * {@inheritDoc}
+     */
+    @Override
+    public DelegatedExecutionOsgiBundleApplicationContext createApplicationContext(BundleContext bundleContext) throws Exception {
+        DelegatedExecutionOsgiBundleApplicationContext applicationContext = super.createApplicationContext(bundleContext);
+        return applicationContext;
+    }
+
+}
