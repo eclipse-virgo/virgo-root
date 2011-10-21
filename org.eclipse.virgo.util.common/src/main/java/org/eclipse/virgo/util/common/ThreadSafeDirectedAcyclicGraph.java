5a0afb416fee4f5d123059424b032c1c528704fd
Florian Waibel <fwaibel@eclipsesource.com>
2011-10-21 20:11:07 +0200

    ASSIGNED - bug 358697: [Subsystems] DAG interface and implementation https://bugs.eclipse.org/bugs/show_bug.cgi?id=358697 - Added the appropriate license header to new files.

--- org.eclipse.virgo.util.common/src/main/java/org/eclipse/virgo/util/common/ThreadSafeDirectedAcyclicGraph.java
+++ org.eclipse.virgo.util.common/src/main/java/org/eclipse/virgo/util/common/ThreadSafeDirectedAcyclicGraph.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2011 VMware Inc. and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   VMware Inc. - initial contribution (ThreadSafeArrayListTree.java)
+ *   EclipseSource - reworked from generic tree to DAG (Bug 358697)
+ *******************************************************************************/
 
 package org.eclipse.virgo.util.common;
 
--- org.eclipse.virgo.util.common/src/test/java/org/eclipse/virgo/util/common/ThreadSafeGraphNodeTests.java
+++ org.eclipse.virgo.util.common/src/test/java/org/eclipse/virgo/util/common/ThreadSafeGraphNodeTests.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2010 VMware Inc.
+ * Copyright (c) 2011 VMware Inc. and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   VMware Inc. - initial contribution
+ *   VMware Inc. - initial contribution (ThreadSafeArrayListTreeTests.java)
+ *   EclipseSource - reworked from generic tree to DAG (Bug 358697)
  *******************************************************************************/
 
 package org.eclipse.virgo.util.common;
