b8bf4a202942f1defbbf8aec8f884e635734fdf7
Florian Waibel <fwaibel@eclipsesource.com>
2011-11-18 22:22:53 +0100

    Bug 358442 Moved InstallArtifactTreeInclosure to InstallArtifactGraphInclosure

--- org.eclipse.virgo.kernel.deployer/src/main/java/org/eclipse/virgo/kernel/install/artifact/InstallArtifactGraphInclosure.java
+++ org.eclipse.virgo.kernel.deployer/src/main/java/org/eclipse/virgo/kernel/install/artifact/InstallArtifactGraphInclosure.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2010 VMware Inc.
+ * Copyright (c) 2008, 2010, 2011 VMware Inc. and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,20 +7,20 @@
  *
  * Contributors:
  *   VMware Inc. - initial contribution
+ *   EclipseSource - Bug 358442 Change InstallArtifact graph from a tree to a DAG
  *******************************************************************************/
 
 package org.eclipse.virgo.kernel.install.artifact;
 
 import java.io.File;
 
-
 import org.eclipse.virgo.kernel.artifact.ArtifactSpecification;
 import org.eclipse.virgo.kernel.deployer.core.DeploymentException;
 import org.eclipse.virgo.kernel.deployer.core.DeploymentOptions;
-import org.eclipse.virgo.util.common.Tree;
+import org.eclipse.virgo.util.common.GraphNode;
 
 /**
- * {@link InstallArtifactTreeInclosure} is used to create, and store persistently, various types of
+ * {@link InstallArtifactGraphInclosure} is used to create, and store persistently, various types of
  * {@link InstallArtifact}.
  * <p />
  * 
@@ -29,49 +29,49 @@ import org.eclipse.virgo.util.common.Tree;
  * This class is thread safe.
  * 
  */
-public interface InstallArtifactTreeInclosure {
+public interface InstallArtifactGraphInclosure {
 
     /**
-     * Create an install tree consisting of the single artifact matching the supplied {@link ArtifactSpecification}.
+     * Create an install graph consisting of the single artifact matching the supplied {@link ArtifactSpecification}.
      * 
      * @param artifactSpecification the <code>ArtifactSpecification</code>.
-     * @return an install tree
-     * @throws DeploymentException if the tree cannot be created
+     * @return an install graph
+     * @throws DeploymentException if the graph cannot be created
      */
-    Tree<InstallArtifact> createInstallTree(ArtifactSpecification artifactSpecification) throws DeploymentException;
+    GraphNode<InstallArtifact> createInstallGraph(ArtifactSpecification artifactSpecification) throws DeploymentException;
 
     /**
-     * Create an install tree consisting of the single artifact matching the supplied {@link ArtifactSpecification}. if
+     * Create an install graph consisting of the single artifact matching the supplied {@link ArtifactSpecification}. if
      * the given scope name is non-<code>null</code>, the installation is scoped. If the given scope name is
-     * <code>null</code>, the behaviour of this method is equivalent to that of the <code>createInstallTree</code>
+     * <code>null</code>, the behaviour of this method is equivalent to that of the <code>createInstallGraph</code>
      * method with no scope name parameter.
      * 
      * @param artifactSpecification the <code>ArtifactSpecification</code>.
      * @param scopeName the scope name of the artifact or <code>null</code> if it does not belong to a scope
-     * @return an install tree
-     * @throws DeploymentException if the tree cannot be created
+     * @return an install graph
+     * @throws DeploymentException if the graph cannot be created
      */
-    Tree<InstallArtifact> createInstallTree(ArtifactSpecification artifactSpecification, String scopeName) throws DeploymentException;
+    GraphNode<InstallArtifact> createInstallGraph(ArtifactSpecification artifactSpecification, String scopeName) throws DeploymentException;
 
     /**
-     * Create an install tree consisting of the single artifact available at the given file URI.
+     * Create an install graph consisting of the single artifact available at the given file URI.
      * 
      * @param location the artifact's location
-     * @return an install tree
-     * @throws DeploymentException if the tree cannot be created
+     * @return an install graph
+     * @throws DeploymentException if the graph cannot be created
      */
-    Tree<InstallArtifact> createInstallTree(File location) throws DeploymentException;
+    GraphNode<InstallArtifact> createInstallGraph(File location) throws DeploymentException;
 
     /**
-     * Optionally recover an install tree from the staging area using the given file URI to identify the artifact. The
+     * Optionally recover an install graph from the staging area using the given file URI to identify the artifact. The
      * source URI need no longer be valid unless the artifact is owned by the deployer. Non-recoverable artifacts are
      * "recovered" by deleting them.
      * 
      * @param location the source location of the artifact
      * @param options the {@link DeploymentOptions} of the artifact
-     * @return an install tree or <code>null</code>
+     * @return an install graph or <code>null</code>
      */
-    Tree<InstallArtifact> recoverInstallTree(File location, DeploymentOptions options);
+    GraphNode<InstallArtifact> recoverInstallGraph(File location, DeploymentOptions options);
 
     /**
      * Update the copy of the given artefact in the deploy area.
