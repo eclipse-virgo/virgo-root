02a10c43d0126d900a630c82d1c77f9f0aba123e
Florian Waibel <fwaibel@eclipsesource.com>
2011-11-18 21:54:15 +0100

    Bug 358442 Added ThreadSafeInstallArtifactGraphFactory

---
+++ org.eclipse.virgo.kernel.deployer/src/main/java/org/eclipse/virgo/kernel/install/artifact/internal/ThreadSafeInstallArtifactGraphFactory.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2011 EclipseSource
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   EclipseSource - Bug 358442 Change InstallArtifact graph from a tree to a DAG
+ *******************************************************************************/
+package org.eclipse.virgo.kernel.install.artifact.internal;
+
+import org.eclipse.virgo.kernel.install.artifact.InstallArtifact;
+import org.eclipse.virgo.util.common.DirectedAcyclicGraph;
+import org.eclipse.virgo.util.common.ThreadSafeDirectedAcyclicGraph;
+
+/**
+ * {@link ThreadSafeInstallArtifactGraphFactory} is used to create the graph of
+ * {@link InstallArtifact InstallArtifacts}.
+ * <p />
+ * 
+ * <strong>Concurrent Semantics</strong><br />
+ * 
+ * Implementations of this interface must be thread safe.
+ * 
+ */
+// TODO - DAG think about a better name
+public class ThreadSafeInstallArtifactGraphFactory {
+
+	private static final DirectedAcyclicGraph<InstallArtifact> DAG = new ThreadSafeDirectedAcyclicGraph<InstallArtifact>();
+
+	public static DirectedAcyclicGraph<InstallArtifact> createInstance() {
+		return DAG;
+	}
+
+}
