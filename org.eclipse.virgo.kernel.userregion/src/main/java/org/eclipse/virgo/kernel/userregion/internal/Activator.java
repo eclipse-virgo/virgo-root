062f3e8fdc42a6ac9f5f3244d89b19a5a7e5e7ae
Borislav Kapukaranov <b.kapukaranov@gmail.com>
2012-01-12 19:54:54 +0200

    351320 - Unites the user region console properties into a single file. The kernel's configuration is inherited from Nano.

--- org.eclipse.virgo.kernel.userregion/src/main/java/org/eclipse/virgo/kernel/userregion/internal/Activator.java
+++ org.eclipse.virgo.kernel.userregion/src/main/java/org/eclipse/virgo/kernel/userregion/internal/Activator.java
@@ -137,6 +137,7 @@ public class Activator implements BundleActivator {
 
         context.registerService(ConfigurationDeployer.class, new UserRegionConfigurationDeployer(context), null);
         this.stateDumpMBeanExorter = new StateDumpMBeanExporter(quasiFrameworkFactory);
+        initializeConsoleConfigurationConvertor(context);
     }
 
     /**
@@ -224,6 +225,11 @@ public class Activator implements BundleActivator {
         Thread thread = new Thread(runnable);
         thread.setDaemon(true);
         thread.start();
+    }
+
+    private void initializeConsoleConfigurationConvertor(BundleContext context) {
+        ConsoleConfigurationConvertor consoleConfigurationConvertor = new ConsoleConfigurationConvertor(context);
+        consoleConfigurationConvertor.start();
     }
 
     /**
