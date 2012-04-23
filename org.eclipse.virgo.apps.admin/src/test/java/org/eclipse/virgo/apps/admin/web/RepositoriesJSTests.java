705d982e138566a84ccec93290738f594d31c905
Glyn Normington <gnormington@vmware.com>
2012-04-23 14:15:21 +0100

    376856: test pageInit

--- org.eclipse.virgo.apps.admin/src/test/java/org/eclipse/virgo/apps/admin/web/RepositoriesJSTests.java
+++ org.eclipse.virgo.apps.admin/src/test/java/org/eclipse/virgo/apps/admin/web/RepositoriesJSTests.java
@@ -8,9 +8,11 @@
  * Contributors:
  *   VMware Inc. - initial contribution
  *******************************************************************************/
+
 package org.eclipse.virgo.apps.admin.web;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
@@ -19,22 +21,36 @@ import javax.script.ScriptException;
 import org.eclipse.virgo.apps.admin.web.stubs.objects.Dollar;
 import org.junit.Test;
 
+import sun.org.mozilla.javascript.internal.Context;
+import sun.org.mozilla.javascript.internal.Function;
+import sun.org.mozilla.javascript.internal.Scriptable;
+
 /**
  *
  *
  */
 public class RepositoriesJSTests extends AbstractJSTests {
-	
-	@Test
-	public void testPageinit() throws ScriptException, IOException, NoSuchMethodException{
-		addCommonObjects();
-		readFile("src/main/webapp/js/repositories.js");
 
-		invokePageInit();
-		
-		assertNotNull(Dollar.getAjaxSuccess());
-		
-		//assertTrue("Page ready has not been called", this.commonUtil.isPageReady());
-	}
-	
+    @Test
+    public void testPageinit() throws ScriptException, IOException, NoSuchMethodException {
+        addCommonObjects();
+        readFile("src/main/webapp/js/repositories.js");
+
+        invokePageInit();
+
+        assertNotNull(Dollar.getAjaxSuccess());
+        Dollar.getAjaxSuccess().call(CONTEXT, SCOPE, SCOPE, new Object[] { getTestData() });
+
+        assertTrue("Page ready has not been called", this.commonUtil.isPageReady());
+
+    }
+
+    private Scriptable getTestData() throws IOException {
+
+        readString("var Data = function() {" + "   this.value = {};" + "   this.value.Properties = {};" + "};");
+
+        Function testData = (Function) SCOPE.get("Data", SCOPE);
+        return testData.construct(CONTEXT, SCOPE, Context.emptyArgs);
+    }
+
 }
