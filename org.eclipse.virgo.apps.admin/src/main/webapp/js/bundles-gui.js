f370762ab9b164710ff691673eaf6d56872e4c0c
Christopher Frost <frostc@vmware.com>
2012-06-01 17:43:48 +0100

    wip on the tooltips and navigation

--- org.eclipse.virgo.apps.admin/src/main/webapp/js/bundles-gui.js
+++ org.eclipse.virgo.apps.admin/src/main/webapp/js/bundles-gui.js
@@ -60,14 +60,15 @@ var LayoutManager = function(width, height, dataSource){
 		self.hideAll();
 		self.focused = -1;
 		var bundle;
-		if(self.bundles[bundleId]){
-			bundle = self.bundles[bundleId];
-		}else{
-			bundle = new Bundle(self.paper, self.dataSource.bundles[bundleId], self.displayBundle);
-			self.bundles[bundleId] = bundle;
-		}
 		self.dataSource.updateBundle(bundleId, function(){
+			if(self.bundles[bundleId]){
+				bundle = self.bundles[bundleId];
+			}else{
+				bundle = new Bundle(self.paper, self.dataSource.bundles[bundleId], self.displayBundle);
+				self.bundles[bundleId] = bundle;
+			}
 			self.focused = bundle;
+			bundle.move(Math.round(self.paper.width/2), Math.round(self.paper.height/2));
 			bundle.show();
 			if(self.relationshipType == 'wires'){
 				self.renderWires(bundle);
@@ -75,163 +76,100 @@ var LayoutManager = function(width, height, dataSource){
 				self.renderServices(bundle);
 			}
 		});
-		bundle.move(Math.round(self.paper.width/2), Math.round(self.paper.height/2));
 		if(self.focusListener){
 			self.focusListener(bundleId);
 		}
 	};
 	
 	self.renderWires = function(bundle){
-		var bundleIds = new Array();
+		var bundleIds = {};
 		$.each(bundle.rawBundle.RequiredWires, function(index, wire){
-			bundleIds.push(wire.ProviderBundleId);
+			bundleIds[wire.ProviderBundleId] = 'tooltip';
 		});
-		var topWidth = self.renderBundleRow(bundleIds, -239);
+		var topWidth = self.renderBundleRow(bundleIds, -239, bundle, false);
 		$.each(bundle.rawBundle.ProvidedWires, function(index, wire){
-			bundleIds.push(wire.RequirerBundleId);
+			bundleIds[wire.RequirerBundleId] = 'tooltip';
 		});
-		var bottomWidth = self.renderBundleRow(bundleIds, 239);
+		var bottomWidth = self.renderBundleRow(bundleIds, 239, bundle, true);
 		var newWidth = topWidth < bottomWidth ? bottomWidth : topWidth;
 		newWidth < 900 ? self.paper.setSize(900, self.paper.height) : self.paper.setSize(newWidth, self.paper.height);
 	};
 	
 	self.renderServices = function(bundle){
-		var bundleIds = new Array();
+		var bundleIds = {};
 		$.each(bundle.rawBundle.ServicesInUse, function(index, service){
-			bundleIds.push(service.BundleIdentifier);
+			bundleIds[service.BundleIdentifier] = 'tooltip';
 		});
-		var topWidth = self.renderBundleRow(bundleIds, -239);
+		var topWidth = self.renderBundleRow(bundleIds, -239, bundle, false);
 		$.each(bundle.rawBundle.RegisteredServices, function(index, service){
 			$.each(service.UsingBundles, function(index, bundleId){
-				bundleIds.push(bundleId);
+				bundleIds[bundleId] = 'tooltip';
 			});
 		});
-		var bottomWidth = self.renderBundleRow(bundleIds, 239);
+		var bottomWidth = self.renderBundleRow(bundleIds, 239, bundle, true);
 		var newWidth = topWidth < bottomWidth ? bottomWidth : topWidth;
 		newWidth < 900 ? self.paper.setSize(900, self.paper.height) : self.paper.setSize(newWidth, self.paper.height);
 	};
 
-	self.renderBundleRow = function(bundleIds, offset){
+	self.renderBundleRow = function(bundleIds, offset, focused, isFrom){
 		var yPos = Math.round(self.paper.height/2) + offset;
 		var xPos = this.bundleSpacing;
 		var bundle;
-		$.each(bundleIds, function(index, bundleId){
+		$.each(bundleIds, function(bundleId, tooltip){
 			if(self.bundles[bundleId]){
 				bundle = self.bundles[bundleId];
 			}else{
-				bundle = new Bundle(self.paper, self.dataSource.bundles[bundleId], self.displayBundle);
+				bundle = new Bundle(self.paper, self.dataSource.bundles[bundleId], tooltip, self.displayBundle);
 				self.bundles[bundleId] = bundle;
 			}
 			if(!bundle.isVisible){
-				xPos = xPos + Math.round(bundle.boxWidth/2);
+				var relationship;
+				if(isFrom){
+					relationship = new Relationship(self.paper, focused, bundle, self.getRelationshipKey(focused, bundle));//, relation.info, relation.infoKey, relation.tooltip);
+				}else{
+					relationship = new Relationship(self.paper, bundle, focused, self.getRelationshipKey(bundle, focused));//, relation.info, relation.infoKey, relation.tooltip);
+				}
+				//bundle.relationships[relationship.key] = relationship;
+				//focused.relationships[relationship.key] = relationship;
+				self.relationships[relationship.key] = relationship;
+				xPos = xPos + bundle.boxWidth/2;
 				bundle.move(xPos, yPos);
 				bundle.show();
-				xPos = xPos + Math.round(bundle.boxWidth/2) + self.bundleSpacing;
+				relationship.display();
+				xPos = xPos + bundle.boxWidth/2 + self.bundleSpacing;
 			}else{
-				
+//				var relationshipKey;
+//				if(isFrom){
+//					relationshipKey = self.getRelationshipKey(focused, bundle);
+//				}else{
+//					relationshipKey = self.getRelationshipKey(bundle, focused);
+//				}
+//				console.log('already visible' + relationshipKey);
 			}
 		});
 		return xPos;
 	};
-
-//	self.empty = function(){
-//		self.hideAll();
-//		self.bundles = {};
-//	};
+	
+	self.getRelationshipKey = function(from, to){
+		return 'from' + from.rawBundle.Identifier + 'to' + to.rawBundle.Identifier;
+	};
 
 	self.hideAll = function(){
 		$.each(self.bundles, function(index, value){
 			value.hide();
 		});
+		$.each(self.relationships, function(index, value){
+			value.remove();
+		});
+		self.relationships = {};
 	};
-	
-//	
-//	this.shuffle = function(bundleId, newRelationships){
-//		$.each(this.relationships, function(index, oldRelationship){
-//			oldRelationship.remove();
-//		});
-//		this.relationships = {};
-//		this.hideAll();
-//
-//		console.log("In", newRelationships[0].length);
-//		console.log("Out", newRelationships[1].length);
-//		
-//		var widthTop = this.renderBundlesRow(this.bundles[bundleId], false, newRelationships[0], -239);
-//		var widthBottom = this.renderBundlesRow(this.bundles[bundleId], true, newRelationships[1], 239);
-//		
-//		var newWidth = widthTop < widthBottom ? widthBottom : widthTop;
-//		newWidth < 900 ? paper.setSize(900, paper.height) : paper.setSize(newWidth, paper.height);
-//		this.bundles[bundleId].move((paper.width/2).round(), (paper.height/2).round());
-//		this.bundles[bundleId].show();
-//		
-//		new Fx.Scroll($('bundle-canvas')).set((paper.width/2).round() - 450, (paper.height/2).round());
-//
-//		$.each(this.relationships, function(index, relationship){
-//			relationship.display();
-//		});
-//		
-//		$('display').setStyle('visibility', 'visible');
-//	};
-//	
-//	
-//
-//	this.renderBundlesRow = function(focused, isFrom, relations, offSet){
-//		var yPos = (paper.height/2).round() + offSet;
-//		var xPos = this.bundleSpacing;
-//		relations.each(function(relation){
-//			if(relation.bundle.id == focused.id){
-//				var relationship = new SelfRelationship(focused, relation.info, relation.infoKey, relation.tooltip);
-//				this.relationships[relationship.key] = relationship;
-//				focused.addRelationship(relationship);
-//			} else {
-//				if(relation.bundle.isVisible){
-//					if(isFrom){
-//						var relationship = new Relationship(focused, relation.bundle, relation.info, relation.infoKey, relation.tooltip);
-//					}else {
-//						var relationship = new Relationship(relation.bundle, focused, relation.info, relation.infoKey, relation.tooltip);
-//					}
-//					var existingRelationship = this.relationships[relationship.key];
-//					if(existingRelationship){
-//						existingRelationship.increaseCount(relation.tooltip);
-//					} else {
-//						console.log('Extra', isFrom, relation.tooltip);
-//						
-//						if(isFrom){
-//							var backRelationship = new Relationship(focused, relation.bundle, relation.info, relation.infoKey, relation.tooltip);
-//						}else{
-//							var backRelationship = new Relationship(relation.bundle, focused, relation.info, relation.infoKey, relation.tooltip);
-//						}
-//						
-//						
-//						this.relationships[backRelationship.key] = backRelationship;
-//						focused.addRelationship(backRelationship);
-//						relation.bundle.addRelationship(backRelationship);
-//					}
-//				} else {
-//					xPos = xPos + (relation.bundle.boxWidth/2);
-//					relation.bundle.move(xPos, yPos);
-//					relation.bundle.show();
-//					xPos = xPos + (relation.bundle.boxWidth/2) + this.bundleSpacing;
-//					if(isFrom){
-//						var relationship = new Relationship(focused, relation.bundle, relation.info, relation.infoKey, relation.tooltip);
-//					}else{
-//						var relationship = new Relationship(relation.bundle, focused, relation.info, relation.infoKey, relation.tooltip);
-//					}
-//					this.relationships[relationship.key] = relationship;
-//					focused.addRelationship(relationship);
-//					relation.bundle.addRelationship(relationship);
-//				}
-//			}
-//		}, this);
-//		return xPos;
-//	};
 	
 };
 
 /**
  * Bundle
  */
-var Bundle = function(paper, rawBundle, dblClickCallback){
+var Bundle = function(paper, rawBundle, tooltip, dblClickCallback){
 
 	var self = this;
 	
@@ -297,108 +235,102 @@ var Bundle = function(paper, rawBundle, dblClickCallback){
 		
 	};
 
-	self.addRelationship = function(relationship){
-		self.relationships[relationship.key] = relationship;
-	};
-
-	self.removeRelationship = function(relationshipKey){
-		Object.erase(self.relationships, relationshipKey);
-	};
-
 };
 
-var Relationship = function(fromBundle, toBundle, infoCallback, infoKey, tooltip) {
+var Relationship = function(paper, fromBundle, toBundle, key){ //}, infoCallback, infoKey, tooltip) {
 
 	var self = this;
 	
-	this.fromBundle = fromBundle;
-	this.toBundle = toBundle;
-	this.infoCallback = infoCallback;
-	this.infoKey = infoKey;
-	this.tooltip = tooltip;
-	this.count = 1;
-	this.key = 'from' + this.fromBundle.id + 'to' + this.toBundle.id;
-	this.controlPointOffset = 100;
+	self.paper = paper;
+	self.fromBundle = fromBundle;
+	self.toBundle = toBundle;
+	self.infoCallback = infoCallback;
+	self.infoKey = infoKey;
+	self.tooltip = tooltip;
+	self.count = 1;
+	self.key = key;
+	self.controlPointOffset = 100;
 
-	this.setCoordinates = function(){
-		this.startPoint = {'x' : fromBundle.x, 'y' : fromBundle.y + fromBundle.boxHeight/2};
-		this.endPoint = {'x' : toBundle.x, 'y' : toBundle.y - toBundle.boxHeight/2};
-		this.startPointControl = {'x' : this.startPoint.x, 'y' : this.startPoint.y + this.controlPointOffset}; 
-		this.endPointControl = {'x' : this.endPoint.x, 'y' : this.endPoint.y - this.controlPointOffset};
-		this.midPoint = this.calculateMidpoint(this.startPoint.x, this.startPoint.y, this.endPoint.x, this.endPoint.y); 
+	self.setCoordinates = function(){
+		self.startPoint = {'x' : fromBundle.x, 'y' : fromBundle.y + fromBundle.boxHeight/2};
+		self.endPoint = {'x' : toBundle.x, 'y' : toBundle.y - toBundle.boxHeight/2};
+		self.startPointControl = {'x' : self.startPoint.x, 'y' : self.startPoint.y + self.controlPointOffset}; 
+		self.endPointControl = {'x' : self.endPoint.x, 'y' : self.endPoint.y - self.controlPointOffset};
+		self.midPoint = self.calculateMidpoint(this.startPoint.x, self.startPoint.y, self.endPoint.x, self.endPoint.y); 
 	};
 	
-	this.calculateMidpoint = function(startX, startY, endX, endY){
+	self.calculateMidpoint = function(startX, startY, endX, endY){
+		var midX, midY;
 		if(startX < endX){
-			var midX = startX + (endX - startX)/2;
+			midX = startX + (endX - startX)/2;
 		} else {
-			var midX = endX + (startX - endX)/2;
+			midX = endX + (startX - endX)/2;
 		}
 		if(startY < endY){
-			var midY = startY + (endY - startY)/2;
+			midY = startY + (endY - startY)/2;
 		} else {
-			var midY = endY + (startY - endY)/2;
+			midY = endY + (startY - endY)/2;
 		}
 		return {'x' : midX, 'y' : midY};
 	};
 	
-	this.display = function() {
-		if(this.visual){
-			this.visual.remove();
+	self.display = function() {
+		if(self.visual){
+			self.visual.remove();
 		}
-		if(this.infoPoint){
-			this.infoPoint.remove();
+		if(self.infoPoint){
+			self.infoPoint.remove();
 		}
-		if(this.infoPointText){
-			this.infoPointText.remove();
+		if(self.infoPointText){
+			self.infoPointText.remove();
 		}
-		this.setCoordinates();
-		this.visual = paper.path('M' + this.startPoint.x + ',' + this.startPoint.y + 
-									'C' + this.startPointControl.x + ',' + this.startPointControl.y + 
-									',' + this.endPointControl.x + ',' + this.endPointControl.y + 
-									',' + this.endPoint.x + ',' + this.endPoint.y).attr({
+		self.setCoordinates();
+		self.visual = self.paper.path('M' + self.startPoint.x + ',' + self.startPoint.y + 
+									'C' + self.startPointControl.x + ',' + self.startPointControl.y + 
+									',' + self.endPointControl.x + ',' + self.endPointControl.y + 
+									',' + self.endPoint.x + ',' + self.endPoint.y).attr({
 			'arrow-end' : 'block-wide-long',
 			'stroke-width' : 3,
 			'stroke' : '#002F5E'
 		}).toBack();
-		this.infoPoint = paper.circle(this.midPoint.x, this.midPoint.y, 10).attr({
+		self.infoPoint = self.paper.circle(self.midPoint.x, self.midPoint.y, 10).attr({
 			'fill' : '#BAD9EC', 
 			'stroke' : 'none',
-			'title' : this.tooltip
+			'title' : self.tooltip
 		});
-		this.infoPointText = paper.text(this.midPoint.x, this.midPoint.y, this.count).attr({
+		self.infoPointText = self.paper.text(self.midPoint.x, self.midPoint.y, self.count).attr({
 			'font' : '13px Arial', 
 			'stroke' : '#002F5E',
-			'title' : this.tooltip
+			'title' : self.tooltip
 		});
 		
-		this.infoPoint.click(function(){this.displayInfoBox();});
-		this.infoPointText.click(function(){this.displayInfoBox();});
+		self.infoPoint.click(function(){self.displayInfoBox();});
+		self.infoPointText.click(function(){self.displayInfoBox();});
 		
-		this.infoPoint.hover(function(){this.glow = this.visual.glow();}, function(){this.glow.remove();}, this, this);
-		this.infoPointText.hover(function(){this.glow = this.visual.glow();}, function(){this.glow.remove();}, this, this);
+		self.infoPoint.hover(function(){self.glow = self.visual.glow();}, function(){self.glow.remove();}, self, self);
+		self.infoPointText.hover(function(){self.glow = self.visual.glow();}, function(){self.glow.remove();}, self, self);
 	};
 	
-	this.displayInfoBox = function() {
-		infoBox.go(this.infoCallback, this.infoKey);
+	self.displayInfoBox = function() {
+		infoBox.go(self.infoCallback, self.infoKey);
 	};
 	
-	this.increaseCount = function(tooltip) {
-		this.count = this.count + 1;
-		this.tooltip = this.tooltip + ' ' + tooltip;
-		if(this.infoPointText){
-			this.infoPointText.attr({
-				'text' : this.count
+	self.increaseCount = function(tooltip) {
+		self.count = self.count + 1;
+		self.tooltip = self.tooltip + ' ' + tooltip;
+		if(self.infoPointText){
+			self.infoPointText.attr({
+				'text' : self.count
 			});
 		};
 	};
 	
-	this.remove = function() {
-		this.visual.remove();
-		this.infoPoint.remove();
-		this.infoPointText.remove();
-		this.fromBundle.removeRelationship(this.key);
-		this.toBundle.removeRelationship(this.key);
+	self.remove = function() {
+		self.visual.remove();
+		self.infoPoint.remove();
+		self.infoPointText.remove();
+		//self.fromBundle.removeRelationship(self.key);
+		//self.toBundle.removeRelationship(self.key);
 	};
 
 };
