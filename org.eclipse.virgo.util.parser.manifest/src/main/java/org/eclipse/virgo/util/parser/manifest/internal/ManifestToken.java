3ce92e9a59dce047392dd49c1b9b9cd292de3c7a
Christopher Frost <frostc@vmware.com>
2012-08-09 18:17:29 +0100

    Removing excess lines and adding comments

--- org.eclipse.virgo.util.parser.manifest/src/main/java/org/eclipse/virgo/util/parser/manifest/internal/ManifestToken.java
+++ org.eclipse.virgo.util.parser.manifest/src/main/java/org/eclipse/virgo/util/parser/manifest/internal/ManifestToken.java
@@ -11,17 +11,15 @@
 
 package org.eclipse.virgo.util.parser.manifest.internal;
 
-
-
+/**
+ * Represents part of a single header from a bundle manifest
+ *
+ */
 public final class ManifestToken implements Token {
-
-
 
 	private static final String COLON_VALUE = ":";
 
 	private static final String NEWLINE_VALUE = "\\n";
-
-
 
 	private String value;
 
@@ -31,101 +29,51 @@ public final class ManifestToken implements Token {
 
 	private int endoffset;
 
-
-
 	public static ManifestToken makeName(char[] value, int start, int end) {
-
 		return new ManifestToken(new String(value), TokenKind.NAME, start, end);
-
 	}
-
-
 
 	public static ManifestToken makeValue(char[] value, int start, int end) {
-
 		return new ManifestToken(new String(value), TokenKind.VALUE, start, end);
-
 	}
-
-
 
 	public static ManifestToken makeColon(int start, int end) {
-
 		return new ManifestToken(COLON_VALUE, TokenKind.COLON, start, end);
-
 	}
-
-
 
 	public static Token makeNewline(int start, int end) {
-
 		return new ManifestToken(NEWLINE_VALUE, TokenKind.NEWLINE, start, end);
-
 	}
-
-
 
 	private ManifestToken(String value, TokenKind kind, int start, int end) {
-
 		this.value = value;
-
 		this.kind = kind;
-
 		this.startoffset = start;
-
 		this.endoffset = end;
-
 	}
-
-
 
 	public String value() {
-
 		return value;
-
 	}
-
-
 
 	@Override
-
 	public String toString() {
-
 		StringBuilder s = new StringBuilder();
-
 		s.append("'").append(value).append("'");
-
 		s.append("@").append(startoffset).append(":").append(endoffset);
-
 		return s.toString();
-
 	}
-
-
 
 	public TokenKind getKind() {
-
 		return kind;
-
 	}
-
-
 
 	public int getEndOffset() {
-
 		return endoffset;
-
 	}
-
-
 
 	public int getStartOffset() {
-
 		return startoffset;
-
 	}
 
-
-
 }
-
