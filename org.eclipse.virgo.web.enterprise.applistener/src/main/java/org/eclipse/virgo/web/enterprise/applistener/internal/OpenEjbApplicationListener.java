9caebca52ae56c6b71ebd54a7f8eb9ba1a4ba1c9
Violeta Georgieva <milesg78@gmail.com>
2012-10-23 09:12:31 +0300

    bug 392630: When destroying application we must use absolute path.

--- org.eclipse.virgo.web.enterprise.applistener/src/main/java/org/eclipse/virgo/web/enterprise/applistener/internal/OpenEjbApplicationListener.java
+++ org.eclipse.virgo.web.enterprise.applistener/src/main/java/org/eclipse/virgo/web/enterprise/applistener/internal/OpenEjbApplicationListener.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 SAP AG
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   SAP AG - initial contribution
+ *******************************************************************************/
 
 package org.eclipse.virgo.web.enterprise.applistener.internal;
 
@@ -16,29 +26,29 @@ import org.eclipse.virgo.web.enterprise.openejb.deployer.VirgoDeployerEjb;
 public class OpenEjbApplicationListener implements LifecycleListener {
 
     public void deploy(StandardContext standardContext) throws Exception {
-    	ServletContext context = standardContext.getServletContext();
+        ServletContext context = standardContext.getServletContext();
         String contextPath = context.getContextPath();
         if (contextPath.equals("")) {
-        	return;
+            return;
         }
         VirgoDeployerEjb deployer = new VirgoDeployerEjb(contextPath, context.getClassLoader());
         try {
-        	
+
             String realPath = context.getRealPath("");
             if (realPath != null) {
                 deployer.deploy(realPath, standardContext);
             } else {
-                deployer.deploy(getAppModuleId(standardContext.getDocBase()), standardContext);              
+                deployer.deploy(getAppModuleId(standardContext.getDocBase()), standardContext);
             }
         } catch (Exception e) {
             // failing to initialise enterprise container should not kill the app's deployment
-            // it might not need enterprise handling 
+            // it might not need enterprise handling
         }
-       
+
     }
-    
+
     public void undeploy(StandardContext standardContext) throws Exception {
-    	ServletContext context = standardContext.getServletContext();
+        ServletContext context = standardContext.getServletContext();
         String contextPath = context.getContextPath();
         VirgoDeployerEjb deployer = new VirgoDeployerEjb(contextPath, context.getClassLoader());
         try {
@@ -46,63 +56,63 @@ public class OpenEjbApplicationListener implements LifecycleListener {
             if (realPath != null) {
                 deployer.undeploy(realPath);
             } else {
-                deployer.undeploy(getAppModuleId(standardContext.getDocBase()));
+                deployer.undeploy(new File(getAppModuleId(standardContext.getDocBase())).getAbsolutePath());
             }
         } catch (Exception e) {
             // failing to destroy enterprise container should not kill the app's undeployment
         }
     }
-    
+
     private String getAppModuleId(String docBase) {
         String appModuleId;
 
-        File appLocation = new File (docBase);
-        if(!appLocation.isAbsolute()) {
+        File appLocation = new File(docBase);
+        if (!appLocation.isAbsolute()) {
             appModuleId = System.getProperty("org.eclipse.virgo.kernel.home") + File.separator + docBase;
         } else {
             appModuleId = docBase;
         }
-        
+
         return appModuleId;
     }
 
-	@Override
-	public void lifecycleEvent(LifecycleEvent event) {
-		Object source = event.getSource();
+    @Override
+    public void lifecycleEvent(LifecycleEvent event) {
+        Object source = event.getSource();
         if (source instanceof StandardContext) {
-			StandardContext standardContext = (StandardContext) source;
-			if (Lifecycle.BEFORE_START_EVENT.equals(event.getType())) {
-			    Loader loader = standardContext.getLoader();
-			    if (loader != null && loader instanceof Lifecycle) {
-			        ((Lifecycle) loader).addLifecycleListener(this);
-			    }
-			} else if (Lifecycle.CONFIGURE_START_EVENT.equals(event.getType())) {
-    			try {
-    				deploy(standardContext);
-    			} catch (Exception e) {
-    				standardContext.setConfigured(false);
-    			}
-    		} else if (Lifecycle.CONFIGURE_STOP_EVENT.equals(event.getType())) {
-    			try {
-    				undeploy(standardContext);
-    			} catch (Exception e) {
-    				standardContext.setConfigured(false);
-    			}
-    		}
-    	} else if (source instanceof Loader) {
-    	    if (Lifecycle.AFTER_START_EVENT.equals(event.getType())) {
-    	        // This event is very important
-    	        // It reorders the lifecycle listeners
-    	        // so that this listener appears after NamingContextListener
-    	        Container container = ((Loader) source).getContainer();
-    	        LifecycleListener[] listeners = container.findLifecycleListeners();
-    	        for (int i = 0; listeners != null && i < listeners.length; i++) {
-    	            if (listeners[i].equals(this)) {
-    	                container.removeLifecycleListener(this);// remove the listener from its current position
-    	            }
-    	        }
-    	        container.addLifecycleListener(this);// add the listener at the end
-    	    }
-    	}
-	}
+            StandardContext standardContext = (StandardContext) source;
+            if (Lifecycle.BEFORE_START_EVENT.equals(event.getType())) {
+                Loader loader = standardContext.getLoader();
+                if (loader != null && loader instanceof Lifecycle) {
+                    ((Lifecycle) loader).addLifecycleListener(this);
+                }
+            } else if (Lifecycle.CONFIGURE_START_EVENT.equals(event.getType())) {
+                try {
+                    deploy(standardContext);
+                } catch (Exception e) {
+                    standardContext.setConfigured(false);
+                }
+            } else if (Lifecycle.CONFIGURE_STOP_EVENT.equals(event.getType())) {
+                try {
+                    undeploy(standardContext);
+                } catch (Exception e) {
+                    standardContext.setConfigured(false);
+                }
+            }
+        } else if (source instanceof Loader) {
+            if (Lifecycle.AFTER_START_EVENT.equals(event.getType())) {
+                // This event is very important
+                // It reorders the lifecycle listeners
+                // so that this listener appears after NamingContextListener
+                Container container = ((Loader) source).getContainer();
+                LifecycleListener[] listeners = container.findLifecycleListeners();
+                for (int i = 0; listeners != null && i < listeners.length; i++) {
+                    if (listeners[i].equals(this)) {
+                        container.removeLifecycleListener(this);// remove the listener from its current position
+                    }
+                }
+                container.addLifecycleListener(this);// add the listener at the end
+            }
+        }
+    }
 }
