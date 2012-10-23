efa9eb7b29ebd5ca3bce6ac8b1a58f9dfa14da42
Violeta Georgieva <milesg78@gmail.com>
2012-10-23 21:43:01 +0300

    bug 392681: OpenEjbSecurityInitializationValve must specify async support

--- org.eclipse.virgo.web.enterprise.security.valve/src/main/java/org/eclipse/virgo/web/enterprise/security/valve/OpenEjbSecurityInitializationValve.java
+++ org.eclipse.virgo.web.enterprise.security.valve/src/main/java/org/eclipse/virgo/web/enterprise/security/valve/OpenEjbSecurityInitializationValve.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2012 SAP AG
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   SAP AG - initial contribution
+ *******************************************************************************/
+
 package org.eclipse.virgo.web.enterprise.security.valve;
 
 import java.io.IOException;
@@ -14,35 +25,37 @@ import org.eclipse.virgo.web.enterprise.security.StandardSecurityService;
 
 public class OpenEjbSecurityInitializationValve extends ValveBase {
 
-	private StandardSecurityService securityService;
+    private StandardSecurityService securityService;
 
-	@Override
-	public void invoke(Request request, Response response) throws IOException,
-			ServletException {
-		Object oldState = null;
-		Wrapper wrapper = (Wrapper) request.getMappingData().wrapper;
-		
-		if (getSecurityService() != null && wrapper != null) {
-			oldState = securityService.enterWebApp(wrapper,
-					request.getPrincipal(), wrapper.getRunAs());
-		}
+    public OpenEjbSecurityInitializationValve() {
+        super(true);
+    }
 
-		try {
-			getNext().invoke(request, response);
-		} finally {
-			if (securityService != null) {
-				securityService.exitWebApp(oldState);
-			}
-		}
+    @Override
+    public void invoke(Request request, Response response) throws IOException, ServletException {
+        Object oldState = null;
+        Wrapper wrapper = (Wrapper) request.getMappingData().wrapper;
 
-	}
-	
-	  private StandardSecurityService getSecurityService() {
-	  	  if(securityService == null) {	  		
-	  		Assembler assembler = (Assembler) SystemInstance.get().getComponent(org.apache.openejb.spi.Assembler.class);
-	  		securityService = (StandardSecurityService)assembler.getSecurityService();
-	  	  }
-	  	  return securityService;
-	    }
+        if (getSecurityService() != null && wrapper != null) {
+            oldState = this.securityService.enterWebApp(wrapper, request.getPrincipal(), wrapper.getRunAs());
+        }
+
+        try {
+            getNext().invoke(request, response);
+        } finally {
+            if (this.securityService != null) {
+                this.securityService.exitWebApp(oldState);
+            }
+        }
+
+    }
+
+    private StandardSecurityService getSecurityService() {
+        if (this.securityService == null) {
+            Assembler assembler = SystemInstance.get().getComponent(org.apache.openejb.spi.Assembler.class);
+            this.securityService = (StandardSecurityService) assembler.getSecurityService();
+        }
+        return this.securityService;
+    }
 
 }
