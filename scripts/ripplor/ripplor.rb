a4f1ef14610ed3fbe3c93aa95817bc131e113912
Christopher Frost <frostc@vmware.com>
2012-12-07 17:37:45 +0000

    395050 - ripplor script should by default clean the Ivy cache for all ripples

--- scripts/ripplor/ripplor.rb
+++ scripts/ripplor/ripplor.rb
@@ -38,19 +38,18 @@ eclipse_repo_root = 'ssh://' + args[:remote_user] + '@git.eclipse.org/gitroot/vi
 ripple_branch = args[:branch_name]
 
 ALL_REPOS = [
-  Repository.new(eclipse_repo_root, 'util',					paths['util'],					'org.eclipse.virgo.util',				nil, ripple_branch, 'clean clean-all-integration clean-ivy test publish-ivy'),
+  Repository.new(eclipse_repo_root, 'util',					paths['util'],					'org.eclipse.virgo.util',				nil, ripple_branch),
   Repository.new(eclipse_repo_root, 'test',					paths['test'],					'org.eclipse.virgo.test',				nil, ripple_branch),
   Repository.new(eclipse_repo_root, 'medic',				paths['medic'],					'org.eclipse.virgo.medic',				nil, ripple_branch),
-  Repository.new(eclipse_repo_root, 'nano',					paths['nano'],					'org.eclipse.virgo.nano',				nil, ripple_branch, 'clean clean-integration test publish-ivy'),
+  Repository.new(eclipse_repo_root, 'nano',					paths['nano'],					'org.eclipse.virgo.nano',				nil, ripple_branch),
   Repository.new(eclipse_repo_root, 'artifact-repository',	paths['artifact-repository'],	'org.eclipse.virgo.repository',			nil, ripple_branch),
-  Repository.new(eclipse_repo_root, 'kernel',				paths['kernel'],				'org.eclipse.virgo.kernel',				nil, ripple_branch, 'clean clean-integration test publish-ivy'),
+  Repository.new(eclipse_repo_root, 'kernel',				paths['kernel'],				'org.eclipse.virgo.kernel',				nil, ripple_branch),
   Repository.new(eclipse_repo_root, 'kernel-tools',			paths['kernel-tools'],			'org.eclipse.virgo.kernel-tools',		nil, ripple_branch),
   Repository.new(eclipse_repo_root, 'web',					paths['web'],					'org.eclipse.virgo.web',				nil, ripple_branch),
   Repository.new(eclipse_repo_root, 'snaps',				paths['snaps'],					'org.eclipse.virgo.snaps',				nil, ripple_branch, 'clean clean-integration test package publish-ivy publish-build'),
   Repository.new(eclipse_repo_root, 'apps',					paths['apps'],					'org.eclipse.virgo.apps',				nil, ripple_branch),
   Repository.new(eclipse_repo_root, 'documentation',		paths['documentation'],			'org.eclipse.virgo.documentation',		nil, ripple_branch, 'clean clean-integration doc-html publish-ivy'),
-  Repository.new(eclipse_repo_root, 'packaging',			paths['packaging'],             'org.eclipse.virgo.packaging',			nil, 
-      ripple_branch, 'clean clean-integration test package smoke-test publish-ivy publish-packages-build')
+  Repository.new(eclipse_repo_root, 'packaging',			paths['packaging'],             'org.eclipse.virgo.packaging',			nil, ripple_branch, 'clean clean-integration test package smoke-test publish-ivy publish-packages-build')
 ]
 
 repos = Array.new
@@ -81,6 +80,10 @@ begin
 	puts '	checkout with "' + repo.clone_command + '"' if DRY_RUN
 	console.set_title(SCRIPT_NAME, "#{repo.name} Checkout")
 	repo.checkout if !DRY_RUN
+	if !DRY_RUN && repo.name == args[:start_repo]
+		puts '  Cleaning the Ivy Cache'
+        system('ant -f ' + repo.path + '/build-*/build.xml clean-all-integration clean-ivy >> ' + log_file)
+	end
 	puts '	update_versions ...' if DRY_RUN
 	console.set_title(SCRIPT_NAME, "#{repo.name} Update")
 	repo.update_versions(versions) if !DRY_RUN
