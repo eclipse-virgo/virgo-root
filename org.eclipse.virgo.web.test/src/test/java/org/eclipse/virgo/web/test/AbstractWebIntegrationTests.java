d3786854310c9265886a0bdea06e5c3774fe039a
Violeta Georgieva <milesg78@gmail.com>
2013-02-26 18:27:56 +0200

    Fix the virgo version in tests

--- org.eclipse.virgo.web.test/src/test/java/org/eclipse/virgo/web/test/AbstractWebIntegrationTests.java
+++ org.eclipse.virgo.web.test/src/test/java/org/eclipse/virgo/web/test/AbstractWebIntegrationTests.java
@@ -66,7 +66,7 @@ public abstract class AbstractWebIntegrationTests {
 
     private static final long WEB_PLAN_DEPLOY_TIMEOUT = 5*60*1000; // 5 minutes
 
-	private static final String CURRENT_VERSION = "3.6.0";
+	private static final String CURRENT_VERSION = "3.7.0";
 
     private static final String USER_REGION_NAME = "org.eclipse.virgo.region.user";
 
