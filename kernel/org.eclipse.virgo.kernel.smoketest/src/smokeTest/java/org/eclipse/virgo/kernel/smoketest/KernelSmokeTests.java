bf870df9b8dc20ecda1a76c681d6d6f9e6a12123
Florian Waibel <fwaibel@eclipsesource.com>
2015-04-16 14:22:51 +0200

    463462 - Switches Virgo Kernel to Gradle build

---
+++ kernel/org.eclipse.virgo.kernel.smoketest/src/smokeTest/java/org/eclipse/virgo/kernel/smoketest/KernelSmokeTests.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2010 VMware Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   VMware Inc. - initial contribution
+ *******************************************************************************/
+
+package org.eclipse.virgo.kernel.smoketest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.virgo.test.tools.JmxUtils;
+import org.eclipse.virgo.test.tools.ServerUtils;
+import org.eclipse.virgo.test.tools.VirgoServerShutdownThread;
+import org.eclipse.virgo.test.tools.VirgoServerStartupThread;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class KernelSmokeTests {
+
+    private static final String VIRGO_FLAVOR = "kernel";
+
+    @BeforeClass
+    public static void initJmxConnection() {
+        JmxUtils.virgoHome = ServerUtils.getHome(VIRGO_FLAVOR);
+    }
+
+    @Before
+    public void startServer() throws Exception {
+        new Thread(new VirgoServerStartupThread(ServerUtils.getBinDir(VIRGO_FLAVOR))).start();
+        JmxUtils.waitForVirgoServerStartFully();
+        Thread.sleep(5000); // wait for startup to complete in case it fails
+
+        assertEquals(JmxUtils.STATUS_STARTED, JmxUtils.getKernelStatus());
+    }
+
+    @After
+    public void shutdownServer() throws Exception {
+        new Thread(new VirgoServerShutdownThread(ServerUtils.getBinDir(VIRGO_FLAVOR))).start();
+        JmxUtils.waitForVirgoServerShutdownFully();
+    }
+
+	@Test
+	public void testKernelStartUpStatus() throws Exception {
+		assertEquals(JmxUtils.STATUS_STARTED, JmxUtils.getKernelStatus());
+	}
+
+}
