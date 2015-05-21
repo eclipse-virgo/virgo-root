92fcbf89406558c432267dc5fa21d7c80855de37
Florian Waibel <fwaibel@eclipsesource.com>
2015-05-21 21:17:01 +0200

    Fixes findbugs warning ST_WRITE_TO_STATIC_FROM_INSTANCE_METHOD in JUnit test

--- org.eclipse.virgo.repository/src/test/java/org/eclipse/virgo/repository/internal/StandardQueryTests.java
+++ org.eclipse.virgo.repository/src/test/java/org/eclipse/virgo/repository/internal/StandardQueryTests.java
@@ -35,97 +35,97 @@ import org.osgi.framework.Version;
  */
 public class StandardQueryTests {
 
-    private static StandardQuery QUERY;
+    private StandardQuery query;
 
-    private static StandardQuery QUERY_PROP;
+    private StandardQuery queryWithAttributeParameters;
 
-    private static StubArtefactDepository ARTEFACT_DEPOSITORY;
+    private StubArtefactDepository artefactDepository;
 
     @Before
     public void setUp() {
-        ARTEFACT_DEPOSITORY = new StubArtefactDepository();
-        QUERY = new StandardQuery(ARTEFACT_DEPOSITORY, "foo", "bar");
-        QUERY_PROP = new StandardQuery(ARTEFACT_DEPOSITORY, "foo", "bar", TEST_ATTRIBUTE_PARAMETERS_THREE);
+        artefactDepository = new StubArtefactDepository();
+        query = new StandardQuery(artefactDepository, "foo", "bar");
+        queryWithAttributeParameters = new StandardQuery(artefactDepository, "foo", "bar", TEST_ATTRIBUTE_PARAMETERS_THREE);
     }
 
     @Test
     public void testNewQuery() {
-        QUERY.run();
-        Set<Attribute> filters = ARTEFACT_DEPOSITORY.getFilters();
+        query.run();
+        Set<Attribute> filters = artefactDepository.getFilters();
         assertEquals("Wrong number of filters found", 1, filters.size());
     }
 
     @Test
     public void testNewQueryWithProps() {
-        QUERY_PROP.run();
-        Set<Attribute> filters = ARTEFACT_DEPOSITORY.getFilters();
+        queryWithAttributeParameters.run();
+        Set<Attribute> filters = artefactDepository.getFilters();
         assertEquals("Wrong number of filters found", 1, filters.size());
         assertEquals("Wring properties found", TEST_ATTRIBUTE_PARAMETERS_THREE, filters.iterator().next().getProperties());
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testAddFilterNullName() {
-        QUERY.addFilter(null, "bananas");
+        query.addFilter(null, "bananas");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testAddFilterNullValue() {
-        QUERY.addFilter("monkey", null);
+        query.addFilter("monkey", null);
     }
 
     @Test
     public void testAddFilter() {
-        QUERY.addFilter("monkey", "bananas");
-        QUERY.run();
-        Set<Attribute> filters = ARTEFACT_DEPOSITORY.getFilters();
+        query.addFilter("monkey", "bananas");
+        query.run();
+        Set<Attribute> filters = artefactDepository.getFilters();
         assertEquals("Wrong number of filters found", 2, filters.size());
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testAddFilterWithParametersNullName() {
-        QUERY.addFilter(null, "bananas", new HashMap<String, Set<String>>(0));
+        query.addFilter(null, "bananas", new HashMap<String, Set<String>>(0));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testAddFilterWithParametersNullValue() {
-        QUERY.addFilter("monkey", null, new HashMap<String, Set<String>>(0));
+        query.addFilter("monkey", null, new HashMap<String, Set<String>>(0));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testAddFilterWithParametersNullParameters() {
-        QUERY.addFilter("monkey", "bananas", null);
+        query.addFilter("monkey", "bananas", null);
     }
 
     @Test
     public void testAddFilterWithParameters() {
-        QUERY.addFilter("monkey", "bananas", new HashMap<String, Set<String>>(0));
-        QUERY.run();
-        Set<Attribute> filters = ARTEFACT_DEPOSITORY.getFilters();
+        query.addFilter("monkey", "bananas", new HashMap<String, Set<String>>(0));
+        query.run();
+        Set<Attribute> filters = artefactDepository.getFilters();
         assertEquals("Wrong number of filters found", 2, filters.size());
     }
 
     @Test
     public void testQueryNoArtefacts() {
-        ARTEFACT_DEPOSITORY.setNextReturnCount(StubArtefactDepository.NONE);
-        Set<RepositoryAwareArtifactDescriptor> artefacts = QUERY.run();
+        artefactDepository.setNextReturnCount(StubArtefactDepository.NONE);
+        Set<RepositoryAwareArtifactDescriptor> artefacts = query.run();
         assertEquals(0, artefacts.size());
     }
 
     @Test
     public void testQueryOneArtefact() {
-        ARTEFACT_DEPOSITORY.setNextReturnCount(StubArtefactDepository.ONE);
-        Set<RepositoryAwareArtifactDescriptor> artefacts = QUERY.run();
+        artefactDepository.setNextReturnCount(StubArtefactDepository.ONE);
+        Set<RepositoryAwareArtifactDescriptor> artefacts = query.run();
         assertEquals(1, artefacts.size());
     }
 
     public void testQueryMultipleArtefacts() {
-        ARTEFACT_DEPOSITORY.setNextReturnCount(StubArtefactDepository.MANY);
-        assertTrue(QUERY.run().size() > 1);
+        artefactDepository.setNextReturnCount(StubArtefactDepository.MANY);
+        assertTrue(query.run().size() > 1);
     }
 
     @Test
     public void testToString() {
-        String toString = QUERY_PROP.toString();
+        String toString = queryWithAttributeParameters.toString();
         assertTrue("toString dosn't contain all the required information", toString.contains("foo=bar"));
         assertTrue("toString dosn't contain all the required information", toString.contains("2"));
     }
