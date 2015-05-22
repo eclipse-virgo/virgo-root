171d040cd64b9cdf05a4ffc0e801e97ae99ba0a1
Florian Waibel <fwaibel@eclipsesource.com>
2015-05-22 11:53:09 +0200

    Removes unused local variables in JUnit tests

--- org.eclipse.virgo.nano.core/src/test/java/org/eclipse/virgo/nano/config/internal/ConfigurationAdminConfigurationInfoTests.java
+++ org.eclipse.virgo.nano.core/src/test/java/org/eclipse/virgo/nano/config/internal/ConfigurationAdminConfigurationInfoTests.java
@@ -22,48 +22,49 @@ import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
 
-import org.eclipse.virgo.test.stubs.service.cm.StubConfigurationAdmin;
 import org.eclipse.virgo.nano.serviceability.Assert.FatalAssertionException;
+import org.eclipse.virgo.test.stubs.service.cm.StubConfigurationAdmin;
 import org.junit.Test;
 import org.osgi.service.cm.Configuration;
 import org.osgi.service.cm.ConfigurationAdmin;
 
 /**
- * This class is for testing {@link ConfigurationAdminConfigurationInfo} class, 
- * an implementation of the {@link ConfigurationInfo} interface.
+ * This class is for testing {@link ConfigurationAdminConfigurationInfo} class, an implementation of the
+ * {@link ConfigurationInfo} interface.
  */
 public class ConfigurationAdminConfigurationInfoTests {
+
     private static final String CONFIG_INFO_TEST_PID = "CUSTOM_PID";
-    
+
     @Test
     public void testGetPid() {
         ConfigurationAdmin stubAdmin = new StubConfigurationAdmin();
         ConfigurationInfo configurationInfo = new ConfigurationAdminConfigurationInfo(stubAdmin, CONFIG_INFO_TEST_PID);
         assertEquals(CONFIG_INFO_TEST_PID, configurationInfo.getPid());
     }
-    
+
     @Test
     public void testGetPropertiesFromEmptyConfiguration() {
         ConfigurationAdmin stubAdmin = new StubConfigurationAdmin();
         ConfigurationInfo configurationInfo = new ConfigurationAdminConfigurationInfo(stubAdmin, CONFIG_INFO_TEST_PID);
         assertEquals("Properties map should be empty.", new HashMap<String, String>(), configurationInfo.getProperties());
     }
-        
+
     @Test
     public void testFailingConfigurationAdmin() throws IOException {
         ConfigurationAdmin mockAdmin = createMock(ConfigurationAdmin.class);
         expect(mockAdmin.getConfiguration(CONFIG_INFO_TEST_PID, null)).andThrow(new IOException("Error trying to load configuration"));
         replay(mockAdmin);
-        
+
         ConfigurationInfo configurationInfo = new ConfigurationAdminConfigurationInfo(mockAdmin, CONFIG_INFO_TEST_PID);
         assertNull(configurationInfo.getProperties());
     }
-    
-    @Test(expected=FatalAssertionException.class)
+
+    @Test(expected = FatalAssertionException.class)
     public void testFailingConstructorWithNullConfigurationAdmin() throws IOException {
-        ConfigurationInfo configurationInfo = new ConfigurationAdminConfigurationInfo(null, CONFIG_INFO_TEST_PID);
+        new ConfigurationAdminConfigurationInfo(null, CONFIG_INFO_TEST_PID);
     }
-    
+
     @Test
     public void testGetPropertiesWithFilledConfiguration() throws IOException {
         ConfigurationAdmin stubAdmin = new StubConfigurationAdmin();
@@ -71,11 +72,11 @@ public class ConfigurationAdminConfigurationInfoTests {
 
         Dictionary<String, String> dict = new Hashtable<String, String>();
         dict.put("key", "value");
-        
+
         config.update(dict);
-                
+
         ConfigurationInfo configurationInfo = new ConfigurationAdminConfigurationInfo(stubAdmin, CONFIG_INFO_TEST_PID);
-        
+
         Map<String, String> expectedMap = new HashMap<String, String>();
         expectedMap.put("service.factoryPid", "CUSTOM_PID");
         expectedMap.put("service.pid", "CUSTOM_PID");
--- org.eclipse.virgo.nano.core/src/test/java/org/eclipse/virgo/nano/config/internal/ovf/OvfPropertiesSourceTests.java
+++ org.eclipse.virgo.nano.core/src/test/java/org/eclipse/virgo/nano/config/internal/ovf/OvfPropertiesSourceTests.java
@@ -20,18 +20,16 @@ import java.util.Properties;
 
 import org.junit.Test;
 
-
 import org.eclipse.virgo.nano.config.internal.ovf.OvfPropertiesSource;
 import org.eclipse.virgo.nano.diagnostics.KernelLogEvents;
 import org.eclipse.virgo.medic.test.eventlog.LoggedEvent;
 import org.eclipse.virgo.medic.test.eventlog.MockEventLogger;
 import org.eclipse.virgo.test.stubs.framework.StubBundleContext;
 
-
 /**
  */
 public class OvfPropertiesSourceTests {
-    
+
     @Test
     public void testReadValidFile() {
         MockEventLogger logger = new MockEventLogger();
@@ -42,19 +40,19 @@ public class OvfPropertiesSourceTests {
         OvfPropertiesSource source = new OvfPropertiesSource(context, logger);
         Map<String, Properties> properties = source.getConfigurationProperties();
         assertNotNull(properties);
-        
+
         Properties one = properties.get("one");
         assertNotNull(one);
         assertEquals("bar", one.getProperty("foo"));
         assertEquals("baz", one.getProperty("bar"));
-        
+
         Properties two = properties.get("two");
         assertNotNull(two);
         assertEquals("quux", two.getProperty("baz"));
-        
+
     }
-    
-    @Test(expected=IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void testReadFileWithInvalidProperty() {
         MockEventLogger logger = new MockEventLogger();
 
@@ -64,7 +62,7 @@ public class OvfPropertiesSourceTests {
         OvfPropertiesSource source = new OvfPropertiesSource(context, logger);
         source.getConfigurationProperties();
     }
-    
+
     @Test
     public void testReadNonExistentFile() {
         MockEventLogger logger = new MockEventLogger();
@@ -73,11 +71,11 @@ public class OvfPropertiesSourceTests {
         context.addProperty(OvfPropertiesSource.FRAMEWORK_PROPERTY_OVF, "src/test/resources/ovf/nonexistent.xml");
 
         OvfPropertiesSource source = new OvfPropertiesSource(context, logger);
-        Map<String, Properties> properties = source.getConfigurationProperties();
-        
+        source.getConfigurationProperties();
+
         List<LoggedEvent> loggedEvents = logger.getLoggedEvents();
         assertEquals(1, loggedEvents.size());
-        
+
         LoggedEvent loggedEvent = loggedEvents.get(0);
         assertEquals(KernelLogEvents.OVF_CONFIGURATION_FILE_DOES_NOT_EXIST.getEventCode(), loggedEvent.getCode());
     }
