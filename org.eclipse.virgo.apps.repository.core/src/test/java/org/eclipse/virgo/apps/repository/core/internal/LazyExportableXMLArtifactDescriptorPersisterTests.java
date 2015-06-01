205e1832b7f80e23c5f18ab81d2e5ba42c249d31
Florian Waibel <fwaibel@eclipsesource.com>
2015-06-01 21:44:11 +0200

    Reworks inner test classes to static inner classes

--- org.eclipse.virgo.apps.repository.core/src/test/java/org/eclipse/virgo/apps/repository/core/internal/LazyExportableXMLArtifactDescriptorPersisterTests.java
+++ org.eclipse.virgo.apps.repository.core/src/test/java/org/eclipse/virgo/apps/repository/core/internal/LazyExportableXMLArtifactDescriptorPersisterTests.java
@@ -31,11 +31,9 @@ import org.eclipse.virgo.repository.codec.XMLRepositoryCodec;
 import org.eclipse.virgo.repository.internal.StandardArtifactDescriptor;
 import org.eclipse.virgo.util.io.PathReference;
 
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-
 
 /**
  * Tests for {@link LazyExportableXMLArtifactDescriptorPersister}
@@ -44,43 +42,45 @@ import static org.junit.Assert.assertNotNull;
  */
 public class LazyExportableXMLArtifactDescriptorPersisterTests {
 
-    
     private File solePoolFile = new File("build/lazypersistertest/file001");
+
     private File nextPoolFile = new File("build/lazypersistertest/file002");
-    
+
     @Test
     public void persistAndLoadArtifacts() throws Exception {
-        
+
         RepositoryCodec repositoryCodec = new XMLRepositoryCodec();
 
         new PathReference(solePoolFile).createFile();
         new PathReference(nextPoolFile).createFile();
         FilePool filePool = new StubFilePool(solePoolFile, nextPoolFile);
-        
+
         ExportingArtifactDescriptorPersister persister = new LazyExportableXMLArtifactDescriptorPersister("repo-name", repositoryCodec, filePool);
-        
+
         Set<RepositoryAwareArtifactDescriptor> testSet = generateDescriptorSet();
-        
+
         persister.persistArtifactDescriptors(testSet);
-        
+
         Set<RepositoryAwareArtifactDescriptor> set = persister.loadArtifacts();
-        
-        assertFalse("Sets are identical -- persister should copy them", testSet==set);
-        
+
+        assertFalse("Sets are identical -- persister should copy them", testSet == set);
+
         assertEquals("Set of descriptors returned is not the same as the set put in", testSet, set);
     }
-    
+
     private Set<RepositoryAwareArtifactDescriptor> generateDescriptorSet() {
         Set<RepositoryAwareArtifactDescriptor> setDescriptors = new HashSet<RepositoryAwareArtifactDescriptor>();
-        RepositoryAwareArtifactDescriptor raad1 = new StubRepositoryAwareArtifactDescriptor("repo-name", new File("build/testlazypersister/raad1").toURI(), "bundle", "bundle.raad1", new Version("1.1"), "raad1", new HashSet<Attribute>());
-        RepositoryAwareArtifactDescriptor raad2 = new StubRepositoryAwareArtifactDescriptor("repo-name", new File("build/testlazypersister/raad2").toURI(), "bundle", "bundle.raad2", new Version("1.2"), "raad2", new HashSet<Attribute>());
-        
+        RepositoryAwareArtifactDescriptor raad1 = new StubRepositoryAwareArtifactDescriptor("repo-name",
+            new File("build/testlazypersister/raad1").toURI(), "bundle", "bundle.raad1", new Version("1.1"), "raad1", new HashSet<Attribute>());
+        RepositoryAwareArtifactDescriptor raad2 = new StubRepositoryAwareArtifactDescriptor("repo-name",
+            new File("build/testlazypersister/raad2").toURI(), "bundle", "bundle.raad2", new Version("1.2"), "raad2", new HashSet<Attribute>());
+
         setDescriptors.add(raad1);
         setDescriptors.add(raad2);
-        
+
         return setDescriptors;
     }
-    
+
     @Test
     public void exportIndexFile() throws Exception {
         RepositoryCodec repositoryCodec = new XMLRepositoryCodec();
@@ -93,31 +93,34 @@ public class LazyExportableXMLArtifactDescriptorPersisterTests {
 
         Set<RepositoryAwareArtifactDescriptor> testSet = generateDescriptorSet();
         persister.persistArtifactDescriptors(testSet);
-        
+
         File exportedFile = persister.exportIndexFile();
         assertNotNull(exportedFile);
-        
+
         Set<ArtifactDescriptor> decodedOutput = repositoryCodec.read(new FileInputStream(exportedFile));
-        
+
         assertEquals(removeRepositoryAwareness(testSet), decodedOutput);
     }
-    
+
     private static Set<ArtifactDescriptor> removeRepositoryAwareness(Set<RepositoryAwareArtifactDescriptor> raSet) {
         final Set<ArtifactDescriptor> result = new HashSet<ArtifactDescriptor>(raSet.size());
         for (ArtifactDescriptor artifactDescriptor : raSet) {
-            StandardArtifactDescriptor standardArtifactDescriptor = new StandardArtifactDescriptor(artifactDescriptor.getUri(), artifactDescriptor.getType(), artifactDescriptor.getName(), artifactDescriptor.getVersion(), artifactDescriptor.getFilename(), artifactDescriptor.getAttributes());
+            StandardArtifactDescriptor standardArtifactDescriptor = new StandardArtifactDescriptor(artifactDescriptor.getUri(),
+                artifactDescriptor.getType(), artifactDescriptor.getName(), artifactDescriptor.getVersion(), artifactDescriptor.getFilename(),
+                artifactDescriptor.getAttributes());
             result.add(standardArtifactDescriptor);
         }
         return result;
     }
-    
-    class StubRepositoryAwareArtifactDescriptor implements RepositoryAwareArtifactDescriptor {
-        
+
+    private static class StubRepositoryAwareArtifactDescriptor implements RepositoryAwareArtifactDescriptor {
+
         private final ArtifactDescriptor artifactDescriptor;
+
         private final String repositoryName;
 
-        
-        public StubRepositoryAwareArtifactDescriptor(String repositoryName, URI uri, String type, String name, Version version, String filename, Set<Attribute> attributes) {
+        public StubRepositoryAwareArtifactDescriptor(String repositoryName, URI uri, String type, String name, Version version, String filename,
+            Set<Attribute> attributes) {
             this.artifactDescriptor = new StandardArtifactDescriptor(uri, type, name, version, filename, attributes);
             this.repositoryName = repositoryName;
         }
@@ -154,5 +157,5 @@ public class LazyExportableXMLArtifactDescriptorPersisterTests {
             return this.repositoryName;
         }
     }
-        
+
 }
