22f5293d02d78ebcb84b7fb32d2f526ac0fca774
Daniel Marthaler <dmarthaler@gmx.net>
2017-06-15 11:32:56 +0200

    Bug 518093 - Unable to deploy via web interface when file's path is part of the file name

--- org.eclipse.virgo.management.console/src/test/java/org/eclipse/virgo/management/console/UploadServletTests.java
+++ org.eclipse.virgo.management.console/src/test/java/org/eclipse/virgo/management/console/UploadServletTests.java
@@ -10,43 +10,68 @@
  *******************************************************************************/
 package org.eclipse.virgo.management.console;
 
-import static org.junit.Assert.assertNotNull;
-
-import java.io.File;
-import java.io.IOException;
-
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 public class UploadServletTests {
 
-	@Test
-	public void testDoPost() throws IOException {
-		UploadServlet uploadServlet = new UploadServlet();
-		MockHttpServletRequest request = new MockHttpServletRequest("POST", null);
-		uploadServlet.doPost(request, new MockHttpServletResponse());
-	}
-	
-	@Test(expected=RuntimeException.class)
-	public void testDoUploadFail() throws Exception {
-		UploadServlet uploadServlet = new UploadServlet();
-		FileItem fileItem = new DiskFileItem("foo", "json/application", false, "src/test/resources/test.upload", 500, new File("build"));
-		File stagingDir = new File("build");
-		fileItem.getOutputStream();
-		uploadServlet.doUpload(fileItem, stagingDir);
-	}
-	
-	@Test
-	public void testDoUpload() throws Exception {
-		UploadServlet uploadServlet = new UploadServlet();
-		FileItem fileItem = new DiskFileItem("foo", "json/application", false, "test.upload", 500, new File("build"));
-		File stagingDir = new File("src/test/resources");
-		fileItem.getOutputStream();
-		File doUpload = uploadServlet.doUpload(fileItem, stagingDir);
-		assertNotNull(doUpload);
-	}
+    @Test
+    public void testDoPost() throws IOException {
+        UploadServlet uploadServlet = new UploadServlet();
+        MockHttpServletRequest request = new MockHttpServletRequest("POST", null);
+        uploadServlet.doPost(request, new MockHttpServletResponse());
+    }
 
+    @Test(expected = RuntimeException.class)
+    public void testDoUploadFail() throws Exception {
+        UploadServlet uploadServlet = new UploadServlet();
+        FileItem fileItem = new DiskFileItem("foo", "json/application", false, "src/test/resources\\test.upload", 500, new File("build"));
+        File stagingDir = new File("build");
+        fileItem.getOutputStream();
+        uploadServlet.doUpload(fileItem, stagingDir);
+    }
+
+    @Test
+    public void testDoUpload() throws Exception {
+        UploadServlet uploadServlet = new UploadServlet();
+        FileItem fileItem = new DiskFileItem("foo", "json/application", false, "test.upload", 500, new File("build"));
+        File stagingDir = new File("src/test/resources");
+        fileItem.getOutputStream();
+        File doUpload = uploadServlet.doUpload(fileItem, stagingDir);
+        assertNotNull(doUpload);
+        assertTrue("test.upload".equals(doUpload.getName()));
+    }
+
+    @Test
+    public void testDoUploadWithFullPathUnix() throws Exception {
+        UploadServlet uploadServlet = new UploadServlet();
+        FileItem fileItem = new DiskFileItem("foo", "json/application", false, "src/test/resources/test.upload", 500, new File("build"));
+        File stagingDir = new File("src/test/resources");
+        fileItem.getOutputStream();
+        File doUpload = uploadServlet.doUpload(fileItem, stagingDir);
+        System.out.println(doUpload.getName());
+        assertNotNull(doUpload);
+        assertTrue("test.upload".equals(doUpload.getName()));
+    }
+
+
+    @Test
+    public void testDoUploadWithFullPathWin() throws Exception {
+        UploadServlet uploadServlet = new UploadServlet();
+        FileItem fileItem = new DiskFileItem("foo", "json/application", false, "src\\test\\resources\\test.upload", 500, new File("build"));
+        File stagingDir = new File("src/test/resources");
+        fileItem.getOutputStream();
+        File doUpload = uploadServlet.doUpload(fileItem, stagingDir);
+        assertNotNull(doUpload);
+        assertTrue("test.upload".equals(doUpload.getName()));
+    }
 }
