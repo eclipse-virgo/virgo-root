4e6f91f2d8e275e19f0a78b1517ef3e4b83430df
Florian Waibel <fwaibel@eclipsesource.com>
2018-06-12 21:51:50 +0200

    404390 Replace deprecated PackageAdmin in StandardPackageAdminUtil
    This commit replaces the deprecated PackageAdmin with the generic
    FrameworkWiring.

--- kernel/org.eclipse.virgo.kernel.userregion/src/main/java/org/eclipse/virgo/kernel/userregion/internal/equinox/StandardPackageAdminUtil.java
+++ kernel/org.eclipse.virgo.kernel.userregion/src/main/java/org/eclipse/virgo/kernel/userregion/internal/equinox/StandardPackageAdminUtil.java
@@ -11,13 +11,12 @@
 
 package org.eclipse.virgo.kernel.userregion.internal.equinox;
 
-import org.eclipse.osgi.framework.internal.core.PackageAdminImpl;
+import java.util.Arrays;
+
+import org.eclipse.virgo.kernel.osgi.framework.PackageAdminUtil;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
-import org.osgi.service.packageadmin.PackageAdmin;
-
-import org.eclipse.virgo.kernel.osgi.framework.OsgiFrameworkUtils;
-import org.eclipse.virgo.kernel.osgi.framework.PackageAdminUtil;
+import org.osgi.framework.wiring.FrameworkWiring;
 
 /**
  * {@link StandardPackageAdminUtil} is the implementation of {@link PackageAdminUtil}.
@@ -26,21 +25,20 @@ import org.eclipse.virgo.kernel.osgi.framework.PackageAdminUtil;
  * <strong>Concurrent Semantics</strong><br />
  * 
  * This class is thread safe.
- * 
  */
-@SuppressWarnings("deprecation")
 public final class StandardPackageAdminUtil implements PackageAdminUtil {
 
-    private final PackageAdmin packageAdmin;
+    private final BundleContext bundleContext;
 
     public StandardPackageAdminUtil(BundleContext bundleContext) {
-        this.packageAdmin = OsgiFrameworkUtils.getService(bundleContext, PackageAdmin.class).getService();
+        this.bundleContext = bundleContext;
     }
-    
-    /** 
+
+    /**
      * {@inheritDoc}
      */
     public void synchronouslyRefreshPackages(Bundle[] bundles) {
-        ((PackageAdminImpl)this.packageAdmin).refreshPackages(bundles, true, null);        
+        FrameworkWiring frameworkWiring = this.bundleContext.getBundle().adapt(FrameworkWiring.class);
+        frameworkWiring.refreshBundles(Arrays.asList(bundles));
     }
 }
