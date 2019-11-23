a8b32aefcaa5eb148c438dbd8354349e52669093
Florian Waibel <fwaibel@datenkollektiv.de>
2019-11-23 19:01:59 +0100

    Fix Springframework library
    Also reactivate test-app 'spring-capability-tests'

--- run_virgo_for_apache_tomcat.sh
+++ run_virgo_for_apache_tomcat.sh
@@ -7,6 +7,9 @@
 ./gradlew -Dskip.normalize.bundles=true -Dskip.compress.bundles=true -Dskip.local.signing=true -Dci.build.signjars=false web:test-apps:osgi-webapp:jar
 cp web/test-apps/osgi-webapp/build/libs/osgi-webapp-3.8.0.BUILD-SNAPSHOT.jar packaging/tomcat-server/build/install/virgo-tomcat-server/pickup
 
+./gradlew -Dskip.normalize.bundles=true -Dskip.compress.bundles=true -Dskip.local.signing=true -Dci.build.signjars=false web:test-apps:spring-capability-tests:jar
+cp web/test-apps/spring-capability-tests/build/libs/spring-capability-tests-3.8.0.BUILD-SNAPSHOT.jar packaging/tomcat-server/build/install/virgo-tomcat-server/pickup
+
 #./gradlew -Dskip.normalize.bundles=true -Dskip.compress.bundles=true -Dskip.local.signing=true -Dci.build.signjars=false web:test-apps:classpath-context-config-locations:war
 #cp web/test-apps/classpath-context-config-locations/build/libs/classpath-context-config-locations-3.8.0.BUILD-SNAPSHOT.war packaging/tomcat-server/build/install/virgo-tomcat-server/pickup
 
--- web/test-apps/spring-capability-tests/src/main/java/sct/ApplicationContextNamespaceTestServlet.java
+++ web/test-apps/spring-capability-tests/src/main/java/sct/ApplicationContextNamespaceTestServlet.java
@@ -11,32 +11,35 @@
 
 package sct;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import org.springframework.web.context.support.XmlWebApplicationContext;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.context.support.XmlWebApplicationContext;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ApplicationContextNamespaceTestServlet extends HttpServlet {
 
     private static final long serialVersionUID = -3223361902099778097L;
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        XmlWebApplicationContext ctx = new XmlWebApplicationContext();
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        ctx.setClassLoader(loader);
-        ctx.setConfigLocation("classpath:sct/namespace.xml");
-        ctx.refresh();
-        Object bean = ctx.getBean("someBean");
-        if (bean != null) {
-            PrintWriter writer = resp.getWriter();
-            writer.write("OK");
-            writer.flush();
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try (XmlWebApplicationContext ctx = new XmlWebApplicationContext()) {
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            ctx.setClassLoader(loader);
+            ctx.setConfigLocation("classpath:sct/namespace.xml");
+            ctx.refresh();
+            Object bean = ctx.getBean("someBean");
+            if (bean != null) {
+                PrintWriter writer = resp.getWriter();
+                writer.write("OK");
+                writer.flush();
+            } else {
+                PrintWriter writer = resp.getWriter();
+                writer.write("Not found!");
+                writer.flush();
+            }
         }
     }
 
