709e0b6a99f64eded6f5ef8a19e9db6c29575b4f
Florian Waibel <fwaibel@datenkollektiv.de>
2020-04-28 19:06:20 +0200

    Bump Spring Framework version to 4.3.27.RELEASE - take two

--- ebr/publish.sh
+++ ebr/publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SPRING_VERSION=4.3.26.RELEASE
+SPRING_VERSION=4.3.27.RELEASE
 
 for module in "test" aop aspects beans context context.support core expression jdbc jms messaging orm oxm transaction web webmvc webmvc.portlet websocket; do
   scp -r /tmp/ivy/org.eclipse.virgo.mirrored/org.springframework.${module}/${SPRING_VERSION} virgoBuild@build.eclipse.org:/shared/rt/virgo/ivy/bundles/release/org.eclipse.virgo.mirrored/org.springframework.${module}
